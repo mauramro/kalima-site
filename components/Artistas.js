0b1d227c31f5f760db9c112c8f90cd4f1404ad21
rudycb23 <35671065+rudycb23@users.noreply.github.com>
2022-01-28 00:14:05 -0600

    add bg, changes on carousel,
    se agrega imagen del vinyl al inicio, se actualizan nombres de componentes, se agregan cambios al carosel, se agregan datos al json test

--- components/Artistas.js
+++ components/Artistas.js
@@ -4,18 +4,21 @@ import artistas from "../testData/artistas";
 
 const Artistas = () => {
   return (
-    <><h2 className="pl-8 text-white text-4xl pt-5 mb-2">Artistas</h2>
-    <div className="flex overflow-x-auto p-5 mb-2 relative">
-      {artistas.map((artista) => (
-        <Artista
-          key={artista.id}
-          id={artista.id}
-          name={artista.name}
-          image={artista.image}
-          alt={artista.alt}
-          desc={artista.desc} />
-      ))}
-    </div></>
+    <>
+      <h2 className="pl-8 text-white text-4xl pt-5 mb-2">Artistas</h2>
+      <div className="flex overflow-x-auto p-5 mb-2 relative">
+        {artistas.map((artista) => (
+          <Artista
+            key={artista.id}
+            id={artista.id}
+            name={artista.name}
+            image={artista.image}
+            alt={artista.alt}
+            desc={artista.desc}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
